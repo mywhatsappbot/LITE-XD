5d2c77a9d2653b958d07fd6e8d5c6b8264dd023b
mywhatsappbot <mywhatsappbot123@gmail.com>
2025-08-02 22:53:13 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -39,8 +39,8 @@ lite({
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363402507750390@newsletter',
-                    newsletterName: 'Malvin Tech',
+                    newsletterJid: '120363422383085384@newsletter',
+                    newsletterName: 'kavidu bot',
                     serverMessageId: 143
                 }
             }
